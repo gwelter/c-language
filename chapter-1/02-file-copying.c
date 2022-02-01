6bcf98b7f26901bfb5bf00a878ae43e351ec8024
Guilherme Welter <guilherme.a.welter@gmail.com>
2022-01-31 22:11:58 -0300

    Characters counting

---
+++ chapter-1/02-file-copying.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    long nc;
+    while ((c = getchar()) != EOF)
+    {
+        nc++;
+    }
+
+    printf("\nCharacters count: %ld\n", nc);
+    printf("EOF: %d\n", EOF);
+    printf("EOF == c: %d\n", EOF == c);
+
+    return 0;
+}
