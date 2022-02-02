a9fc89cfb22b84c5d78f4b0cf5564a116965bd3d
Guilherme Welter <guilherme.a.welter@gmail.com>
2022-02-02 07:20:52 -0300

    Display raw characteres

---
+++ chapter-1/05-display-raw.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+        switch (c)
+        {
+        case '\n':
+            printf("\\n");
+            break;
+
+        case '\b':
+            printf("\\b");
+            break;
+
+        case '\\':
+            putchar('\\');
+            break;
+
+        case '\t':
+            printf("\\t");
+            break;
+
+        default:
+            putchar(c);
+        }
+    }
+
+    return 0;
+}
